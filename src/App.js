ee0b90abce1ee4af4d753750f992ab5182858045
Brent Ahrens <cidida@gmail.com>
2020-10-22 08:26:32 -0700

    home page is complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './sass/_base.scss'
 
 import Nav from './components/Nav/Nav'
 import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../sass/_footer.scss'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h2>Mission Critical Asset Protection, Inc</h2>
+        </div>
+    )
+}
+
+export default Footer
