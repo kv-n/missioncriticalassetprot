c6d991ceca8e1049a260a77cf22bc55bef2e5768
Brent Ahrens <cidida@gmail.com>
2020-10-27 22:28:07 -0700

    adds scroll to top function

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>,
   document.getElementById('root')
